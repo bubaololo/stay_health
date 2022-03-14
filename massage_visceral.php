7470fea0b22be2b03c643af629fcc784b4aa4820
bubaololo <79483014+bubaololo@users.noreply.github.com>
2022-03-14 13:09:49 +0200

    massage_visceral

---
+++ massage_visceral.php
@@ -0,0 +1,191 @@
+<?php
+    include_once($_SERVER['DOCUMENT_ROOT'].'/header.php');
+    ?>
+<div class="massage visceral">
+    <div class="bubbles__blue bubbles">
+        <div class="big__bubble blue_bubble"></div>
+        <div class="small__bubble blue_bubble"></div>
+        <div class="container">
+            <div class="bubbles__wrapper">
+
+                <div class="bubbles__content">
+                    <div class="bubbles__paginate">
+                        <a href="/" class="paginate__link root">
+                            Главная
+                        </a>
+                        <a href="/" class="paginate__link child">
+                            О клинике
+                        </a>
+                        <a href="/" class="paginate__link child">
+                            Висцеральный массаж
+                        </a>
+                    </div>
+                    <div class="bubbles__title">
+                        Висцеральный массаж
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="specialty__wrapper">
+            <aside class="specialty__links">
+                <a href="#" class="specialty_link massage_type1">Взрослый массаж</a>
+                <a href="#" class="specialty_link massage_type2">Классический массаж</a>
+                <a href="#" class="specialty_link massage_type3">Детский массаж</a>
+                <a href="#" class="specialty_link massage_type4">Массаж спины</a>
+                <a href="#" class="specialty_link massage_type5">Массаж ног</a>
+                <a href="#" class="specialty_link massage_type6">Лимфодренажный массаж</a>
+                <a href="#" class="specialty_link massage_type7">Спортивный массаж</a>
+                <a href="#" class="specialty_link massage_type8">Мануальная терапия</a>
+                <a href="#" class="specialty_link massage_type9">Массаж шейного отдела</a>
+                <a href="#" class="specialty_link massage_type10">Остеопатия</a>
+                <a href="#" class="specialty_link massage_type11">Антицеллюлитный массаж</a>
+                <a href="#" class="specialty_link massage_type12">Висцеральный массаж</a>
+            </aside>
+            <div class="specialty__content ">
+                <div class="specialty__top_title">
+                    Висцеральный массаж живота в клинике «Неболейка» представляет собой лечение посредством воздействия на органы, расположенные внутри брюшной полости. В результате осуществляется мануальная коррекция органов, обеспечивающая благоприятный эффект.
+                </div>
+                <img src="img/massage_visceral.jpg" alt="image" class="specialty__img">
+                
+                <div class="specialty__title">
+                    Показания:
+                </div>
+                <ul class="specialty__list">
+                    <li class="call__list_item">
+                        Заболеваниями желудочно кишечного тракта, такими как запоры, панкреатит, хронический гастрит желудка, гепатит, дискинезия желчевыводящих путей, энтерит, колит, холецистит.
+                    </li>
+                    <li class="call__list_item">
+                        Болезнями половой системы, такими как кисты, опущение матки, проблемы с наступлением беременности и другие гинекологические патологии.
+                    </li>
+                    <li class="call__list_item">
+                        Заболеваниями  сердечно-сосудистой системы.
+                    </li>
+                    <li class="call__list_item">
+                        Болезнями, имеющими отношение к мочевыделительной системе, а именно камни в почках, цистит, пиелонефрит, у мужчин - простатит
+                    </li>
+                </ul>
+                <div class="specialty__text">Специалисты хорошо понимают, что функции органов связаны между собой. Их расположение напрямую зависит от общего состояния, в которых находятся связки, надёжно фиксирующие органы к скелету. Смещение органов обусловлено нарушением целостности связочного аппарата.</div>
+                <div class="specialty__cards">
+                    <div class="specialty__title">
+                        Прием ведут:
+                    </div>
+                    <div class="specs__cards_wrapper">
+                        <div class="spec__card spec1">
+                            <div class="spec__card_content">
+                                <div class="spec__card_content_inner">
+                                    <span class="spec__fname">Верещагина</span>
+                                    <br>
+                                    <span class="spec__name">Юлия Андреевна</span>
+                                    <br>
+                                    <span class="spec__skill">
+                                        Педиатр
+                                    </span>
+
+                                    <div class="massage__item_link">
+                                        <a href="spec_card.php" class="card__link">
+                                            подробнее
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="spec__card spec2">
+                            <div class="spec__card_content">
+                                <div class="spec__card_content_inner">
+                                    <span class="spec__fname">Верещагин</span>
+                                    <br>
+                                    <span class="spec__name">Андрей Николаевич</span>
+                                    <br>
+                                    <span class="spec__skill">
+                                        Педиатр
+                                    </span>
+
+                                    <div class="massage__item_link">
+                                        <a href="spec_card.php" class="card__link">
+                                            подробнее
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="specialty__price">
+                    <div class="specialty__title">
+                        Прием ведут:
+                    </div>
+                    <div class="accordeon__list">
+                        <div class="accordeon__list_item">
+                            <div class="accordeon__list_item_left">
+                                <div class="accordeon__list_num">
+                                    1025023
+                                </div>
+                                <div class="accordeon__list_name">
+                                    Первичный прием врача
+                                </div>
+                            </div>
+                            <div class="accordeon__list_item_price">
+                                1000 р.
+                            </div>
+                        </div>
+                        <div class="accordeon__list_item">
+                            <div class="accordeon__list_item_left">
+                                <div class="accordeon__list_num">
+                                    1025023
+                                </div>
+                                <div class="accordeon__list_name">
+                                    Первичный прием врача
+                                </div>
+                            </div>
+                            <div class="accordeon__list_item_price">
+                                1000 р.
+                            </div>
+                        </div>
+                        <div class="accordeon__list_item">
+                            <div class="accordeon__list_item_left">
+                                <div class="accordeon__list_num">
+                                    1025023
+                                </div>
+                                <div class="accordeon__list_name">
+                                    Первичный прием врача
+                                </div>
+                            </div>
+                            <div class="accordeon__list_item_price">
+                                1000 р.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="main__registration">
+        <div class="container">
+            <div class="main__registration_wrapper">
+                <div class="main__registration_inner">
+                    <div class="registration-circle"></div>
+                    <!-- <img src="img/steto.png" alt="stetoscope image" class="main__registration_steto"> -->
+                    <div class="main__registration_content">
+
+                        <div class="main__registration_title">
+                            Записаться на приём
+                        </div>
+                        <div class="main__registration_text">
+                            Оставьте свои данные и наш администратор свяжется с вами в ближайшее время Либо звоните по
+                            телефону <em>+7 (4012) 000-000</em>
+                        </div>
+                        <a href="#" class="main__docs_button button" data-hystmodal="#myModal">
+                            Записаться на приём
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+<?php
+    include_once($_SERVER['DOCUMENT_ROOT'].'/footer.php');
+    ?>
