56563fc095a97f037cbd827d52b2a4edbdc6048a
bubaololo <79483014+bubaololo@users.noreply.github.com>
2022-03-14 14:23:50 +0200

    404

---
+++ 404.php
@@ -0,0 +1,21 @@
+<?php
+    include_once($_SERVER['DOCUMENT_ROOT'].'/header.php');
+    ?>
+
+<div class="_404">
+    <div class="container">
+        <div class="_404_wrapper">
+            <img src="img/404.svg" class="_404_img" alt="">
+        <div class="_404__text">
+        Упс.. у “Неболейки” нет такой страницы...<br>
+Попробуйте еще раз!
+        </div>
+        <a href="index.php" class="_404__button button">
+            Перейти на главную</a>
+        </div>
+    </div>
+</div>
+
+<?php
+    include_once($_SERVER['DOCUMENT_ROOT'].'/footer.php');
+    ?>
